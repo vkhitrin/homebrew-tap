308d202f222f9e6721fa743d1f31fe0537822a5e
Vadim Khitrin <me@vkhitrin.com>
2025-05-03 22:28:45 +0300

    chore(casks): Bump Cosmicding
    v2025.5.1

--- Casks/cosmicding.rb
+++ Casks/cosmicding.rb
@@ -1,9 +1,9 @@
 cask("cosmicding") do
-  version("2025.5.0")
-  sha256("9c3866173660e69f582746f727be96448ccc1b166a52f8b32b2ae8467c5c6fe8")
+  version("2025.5.1")
+  sha256("b0c5e98926327863d913fd912aba312cac1118a06e92c58b9c0b9e8232682fe1")
 
   url("https://github.com/vkhitrin/cosmicding/releases/download/v#{version}/cosmicding-v#{version}.dmg")
-  name("cosmicding")
+  name("Cosmicding")
   desc("linkding companion app")
   homepage("https://github.com/vkhitrin/cosmicding")
 
@@ -12,11 +12,20 @@ cask("cosmicding") do
     strategy(:github_latest)
   end
 
+  caveats(
+    "A workaround for libcosmic to set XDG_DATA_DIRS will be installed, reboot in order to see application icons. Refer to https://github.com/pop-os/libcosmic/discussions/860"
+  )
+
   depends_on(formula: "cosmic-icons-theme")
 
-  app("cosmicding.app")
+  app("Cosmicding.app")
 
   postflight do
+
+    def share
+      "#{HOMEBREW_PREFIX}/share"
+    end
+
     plist_content = <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -42,13 +51,24 @@ cask("cosmicding") do
     File.write(plist_path, plist_content)
 
     system("/bin/launchctl bootstrap gui/#{Process.uid} #{Shellwords.escape(plist_path)}") || true
+
+    system(
+      "/bin/cp #{appdir}/Cosmicding.app/Contents/Resources/icons/hicolor/scalable/apps/com.vkhitrin.cosmicding.svg #{share}/icons/hicolor/scalable/apps/com.vkhitrin.cosmicding.svg"
+    )
+    system(
+      "/bin/cp #{appdir}/Cosmicding.app/Contents/Resources/icons/hicolor/symbolic/apps/com.vkhitrin.cosmicding-symbolic.svg #{share}/icons/hicolor/symbolic/apps/com.vkhitrin.cosmicding-symbolic.svg"
+    )
   end
 
   uninstall(
     launchctl: "com.vkhitirn.setenv.XDG_DATA_DIRS",
     delete: File.expand_path("~/Library/LaunchDaemons/com.vkhitrin.setenv.XDG_DATA_DIRS.plist")
   )
-  caveats(
-    "A workaround for libcosmic to set XDG_DATA_DIRS will be installed, reboot in order to see application icons. Refer to https://github.com/pop-os/libcosmic/discussions/860"
+
+  zap(
+    trash: [
+      "#{HOMEBREW_PREFIX}/share/icons/hicolor/scalable/apps/com.vkhitrin.cosmicding.svg",
+      "#{HOMEBREW_PREFIX}/share/icons/hicolor/symbolic/apps/com.vkhitrin.cosmicding-symbolic.svg"
+    ]
   )
 end
