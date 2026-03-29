3e13d863de397fd83de6c4703f38e166f7b0e025
Vadim Khitrin <me@vkhitrin.com>
2026-03-29 23:59:53 +0300

    fix(Formula): Adapt docker-cli For audit/bump

--- Formula/docker-cli.rb
+++ Formula/docker-cli.rb
@@ -1,14 +1,14 @@
 class DockerCli < Formula
-  version "29.3.0"
-  desc "Docker CLI binary"
+  desc "Command-line client for Docker"
   homepage "https://docker.com"
-  url "https://download.docker.com/mac/static/stable/aarch64/docker-#{version}.tgz"
+  url "https://download.docker.com/mac/static/stable/aarch64/docker-29.3.0.tgz"
   sha256 "bb9de3df0ed22aec01d2cfe495d0429a516ad4cdb189ee85efd8c38dae8e544f"
+  license "Apache-2.0"
+
   livecheck do
     url "https://download.docker.com/mac/static/stable/aarch64/"
-    regex(/docker[._-](\d+\.\d+\.\d+)\.tgz/i)
+    regex(/docker[._-](\d+\.\d+\.\d+)\.t/i)
   end
-  license "Apache-2.0"
 
   conflicts_with cask: "docker-desktop", because: "both install `docker` binaries"
 
@@ -18,12 +18,12 @@ class DockerCli < Formula
   end
 
   def caveats
-     <<~EOS
-     This is a docker CLI only formula!
-     It conflicts with `docker-desktop` cask.
+    <<~EOS
+      This is a docker CLI only formula!
+      It conflicts with `docker-desktop` cask.
 
-     Combine it with 'socktainer' to mimic docker-like workflow!
-     EOS
+      Combine it with 'socktainer' to mimic docker-like workflow!
+    EOS
   end
 
   test do
